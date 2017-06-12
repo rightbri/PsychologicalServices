ad6ceb0c519d4d0e2beb8454096095f69a58188b
Brian Avent <brian.avent@gmail.com>
2017-06-11 19:46:16 -0500

    updated sqlproj

--- src/Web/PsychologicalServices.DB/dbo/Tables/Companies.sql
+++ src/Web/PsychologicalServices.DB/dbo/Tables/Companies.sql
@@ -1,15 +1,29 @@
 ﻿CREATE TABLE [dbo].[Companies] (
-    [CompanyId] INT            IDENTITY (1, 1) NOT NULL,
-    [Name]      NVARCHAR (100) NOT NULL,
-    [IsActive]  BIT            CONSTRAINT [DF_Companies_IsActive] DEFAULT ((1)) NOT NULL,
-    [AddressId] INT            NULL,
-    [Phone]     NVARCHAR (50)  NULL,
-    [Fax]       NVARCHAR (50)  NULL,
-    [Email]     NVARCHAR (100) NULL,
-    [TaxId]     NVARCHAR (50)  NULL,
+    [CompanyId]                     INT            IDENTITY (1, 1) NOT NULL,
+    [Name]                          NVARCHAR (100) NOT NULL,
+    [IsActive]                      BIT            CONSTRAINT [DF_Companies_IsActive] DEFAULT ((1)) NOT NULL,
+    [AddressId]                     INT            NULL,
+    [Phone]                         NVARCHAR (50)  NULL,
+    [Fax]                           NVARCHAR (50)  NULL,
+    [Email]                         NVARCHAR (100) NULL,
+    [TaxId]                         NVARCHAR (50)  NULL,
+    [NewAppointmentTime]            BIGINT         NULL,
+    [NewAppointmentLocationId]      INT            NULL,
+    [NewAppointmentPsychologistId]  INT            NULL,
+    [NewAppointmentPsychometristId] INT            NULL,
+    [NewAppointmentStatusId]        INT            NULL,
+    [NewAssessmentReportStatusId]   INT            NULL,
+    [NewAssessmentSummaryNoteText]  NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Companies] PRIMARY KEY CLUSTERED ([CompanyId] ASC),
-    CONSTRAINT [FK_Companies_Addresses] FOREIGN KEY ([AddressId]) REFERENCES [dbo].[Addresses] ([AddressId])
+    CONSTRAINT [FK_Companies_Addresses] FOREIGN KEY ([AddressId]) REFERENCES [dbo].[Addresses] ([AddressId]),
+    CONSTRAINT [FK_Companies_Addresses1] FOREIGN KEY ([NewAppointmentLocationId]) REFERENCES [dbo].[Addresses] ([AddressId]),
+    CONSTRAINT [FK_Companies_AppointmentStatuses] FOREIGN KEY ([NewAppointmentStatusId]) REFERENCES [dbo].[AppointmentStatuses] ([AppointmentStatusId]),
+    CONSTRAINT [FK_Companies_ReportStatuses] FOREIGN KEY ([NewAssessmentReportStatusId]) REFERENCES [dbo].[ReportStatuses] ([ReportStatusId]),
+    CONSTRAINT [FK_Companies_Users] FOREIGN KEY ([NewAppointmentPsychologistId]) REFERENCES [dbo].[Users] ([UserId]),
+    CONSTRAINT [FK_Companies_Users1] FOREIGN KEY ([NewAppointmentPsychometristId]) REFERENCES [dbo].[Users] ([UserId])
 );
+
+
 
 
 
--- src/Web/PsychologicalServices.DB/dbo/Tables/Notes.sql
+++ src/Web/PsychologicalServices.DB/dbo/Tables/Notes.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [dbo].[Notes] (
-    [NoteId]       INT             IDENTITY (1, 1) NOT NULL,
-    [Note]         NVARCHAR (1000) NOT NULL,
-    [UpdateUserId] INT             NOT NULL,
-    [UpdateDate]   DATETIME        CONSTRAINT [DF_Notes_UpdateDate] DEFAULT (getdate()) NOT NULL,
-    [CreateUserId] INT             NOT NULL,
-    [CreateDate]   DATETIME        CONSTRAINT [DF_Notes_CreateDate] DEFAULT (getdate()) NOT NULL,
+    [NoteId]       INT            IDENTITY (1, 1) NOT NULL,
+    [Note]         NVARCHAR (MAX) NOT NULL,
+    [UpdateUserId] INT            NOT NULL,
+    [UpdateDate]   DATETIME       CONSTRAINT [DF_Notes_UpdateDate] DEFAULT (getdate()) NOT NULL,
+    [CreateUserId] INT            NOT NULL,
+    [CreateDate]   DATETIME       CONSTRAINT [DF_Notes_CreateDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_Notes] PRIMARY KEY CLUSTERED ([NoteId] ASC),
     CONSTRAINT [FK_Notes_CreateUser] FOREIGN KEY ([CreateUserId]) REFERENCES [dbo].[Users] ([UserId]),
     CONSTRAINT [FK_Notes_UpdateUser] FOREIGN KEY ([UpdateUserId]) REFERENCES [dbo].[Users] ([UserId])
 );
+
+
 
 
 
