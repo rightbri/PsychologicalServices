f325b401834e5fc7dbda58c67db58b4814242026
Brian Avent <brian.avent@gmail.com>
2019-07-22 20:45:23 -0500

    update public site

--- src/Web/PsychologicalServices.Js/public/src/home/index.js
+++ src/Web/PsychologicalServices.Js/public/src/home/index.js
@@ -1,36 +1,4 @@
-import {DataRepository} from 'repository/dataRepository';
-import {inject} from 'aurelia-framework';
 
-@inject(DataRepository)
 export class Index {
-    constructor(dataRepository) {
-        this.dataRepository = dataRepository;
 
-        this.noEventsMessage = "Loading upcoming events...";
-
-        this.searchCompanyId = 1;
-        this.searchExpired = false;
-        this.searchActive = true;
-    }
-
-    attached() {
-        return Promise.all([
-			this.getEvents()
-        ]);
-    }
-
-    getEvents() {
-        return this.dataRepository.getEvents({
-            'companyId': this.searchCompanyId,
-            'isExpired': this.searchExpired,
-			'isActive': this.searchActive
-        }).then(data => {
-            this.events = data;
-            //message to be displayed if data is empty
-            this.noEventsMessage = "No upcoming events";
-        }).catch(err => {
-            console.log(err);
-            this.noEventsMessage = "Unable to load events";
-        });
-    }
 }
