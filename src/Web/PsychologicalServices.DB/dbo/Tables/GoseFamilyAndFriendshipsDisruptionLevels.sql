3e9ee69da1e90081e2c5147c5b020cc70f7acf56
Brian Avent <brian.avent@gmail.com>
2020-03-02 20:36:06 -0600

    update gos-e table column widths

--- src/Web/PsychologicalServices.DB/dbo/Tables/GoseFamilyAndFriendshipsDisruptionLevels.sql
+++ src/Web/PsychologicalServices.DB/dbo/Tables/GoseFamilyAndFriendshipsDisruptionLevels.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[GoseFamilyAndFriendshipsDisruptionLevels] (
-    [GoseFamilyAndFriendshipsDisruptionLevelId] INT          IDENTITY (1, 1) NOT NULL,
-    [Description]                               VARCHAR (50) NOT NULL,
+    [GoseFamilyAndFriendshipsDisruptionLevelId] INT           IDENTITY (1, 1) NOT NULL,
+    [Description]                               VARCHAR (100) NOT NULL,
     CONSTRAINT [PK_GoseFamilyAndFriendshipsDisruptionLevels] PRIMARY KEY CLUSTERED ([GoseFamilyAndFriendshipsDisruptionLevelId] ASC)
 );
+
+
 
--- src/Web/PsychologicalServices.DB/dbo/Tables/GoseWorkRestrictionLevels.sql
+++ src/Web/PsychologicalServices.DB/dbo/Tables/GoseWorkRestrictionLevels.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[GoseWorkRestrictionLevels] (
     [GoseWorkRestrictionLevelId] INT           IDENTITY (1, 1) NOT NULL,
-    [Description]                VARCHAR (100) NOT NULL,
+    [Description]                VARCHAR (200) NOT NULL,
     CONSTRAINT [PK_GoseWorkRestrictionLevels] PRIMARY KEY CLUSTERED ([GoseWorkRestrictionLevelId] ASC)
 );
+
+
 
